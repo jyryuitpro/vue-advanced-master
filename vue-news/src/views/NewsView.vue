a9352d81bbf8b451f562e47f8afbbe4db0f1d560
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-08 16:07:00 +0900

    axios를 이용한 api 호출

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    news
+    <div v-for="user in users">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: "NewsView"
+  name: "NewsView",
+  data() {
+    return {
+      users: [],
+    }
+  },
+  created() {
+    var vm = this;
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+    // new Promise()
+        .then(function (response) {
+          console.log(response);
+          vm.users = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+  }
 }
 </script>
 
