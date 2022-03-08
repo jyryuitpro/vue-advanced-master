80c9ca80f6e9400edfd5a2f06ad8d5e7f6c9ca3f
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-08 10:35:42 +0900

    ESLint 도구 소개와 사용해야 하는 이유?

---
+++ vue-news/src/app.js
@@ -0,0 +1,19 @@
+// 1
+var a = 10;
+
+// 2
+var a = 10
+
+import AppHeader from './components/AppHeader'
+import Vue from 'vue'
+
+if (a === 10) {
+    console.log('10이다');
+}
+
+if (a === 1) console.log('10이다')
+
+// 트레일링 콤마 - trailing comma
+components: {
+    // '컴포넌트 이름': 컴포넌트 내용,
+}
