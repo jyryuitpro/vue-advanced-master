e5273ba409124e2ad7a9c266c0273cf391981bbd
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-09 11:50:58 +0900

    스토어 속성 모듈화

---
+++ vue-news/src/store/actions.js
@@ -0,0 +1,33 @@
+import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index.js';
+
+export default {
+    FETCH_NEWS(context) {
+        fetchNewsList()
+            .then(response => {
+                console.log(response.data);
+                context.commit('SET_NEWS', response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+    // 구조 분해 문법(Destructuring)
+    FETCH_JOBS({ commit }) {
+        fetchJobsList()
+            .then(({ data }) => {
+                commit('SET_JOBS', data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+    FETCH_ASK({ commit }) {
+        fetchAskList()
+            .then(({ data }) => {
+                commit('SET_ASK', data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+}
--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index.js';
+// import { fetchNewsList, fetchJobsList, fetchAskList } from '../api/index.js';
 // import { fetchJobsList } from '../api/index.js';
 // import { fetchAskList } from '../api/index.js';
+import mutations from "./mutations.js";
+import actions from "./actions.js";
 
 Vue.use(Vuex);
 
@@ -17,47 +19,51 @@ export const store = new Vuex.Store({
             return state.ask;
         }
     },
-    mutations: {
+    // mutations: mutations,
+    mutations,
+    // mutations: {
         // SET_NEWS: function () {
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        },
-        SET_ASK(state, ask) {
-            state.ask = ask;
-        },
-    },
-    actions: {
-        FETCH_NEWS(context) {
-            fetchNewsList()
-                .then(response => {
-                    console.log(response.data);
-                    context.commit('SET_NEWS', response.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
+        // SET_NEWS(state, news) {
+        //     state.news = news;
+        // },
+        // SET_JOBS(state, jobs) {
+        //     state.jobs = jobs;
+        // },
+        // SET_ASK(state, ask) {
+        //     state.ask = ask;
+        // },
+    // },
+    // actions: actions,
+    actions,
+    // actions: {
+    //     FETCH_NEWS(context) {
+    //         fetchNewsList()
+    //             .then(response => {
+    //                 console.log(response.data);
+    //                 context.commit('SET_NEWS', response.data);
+    //             })
+    //             .catch(error => {
+    //                 console.log(error);
+    //             });
+    //     },
         // 구조 분해 문법(Destructuring)
-        FETCH_JOBS({ commit }) {
-            fetchJobsList()
-                .then(({ data }) => {
-                    commit('SET_JOBS', data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
-        FETCH_ASK({ commit }) {
-            fetchAskList()
-                .then(({ data }) => {
-                    commit('SET_ASK', data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
-    }
+        // FETCH_JOBS({ commit }) {
+        //     fetchJobsList()
+        //         .then(({ data }) => {
+        //             commit('SET_JOBS', data);
+        //         })
+        //         .catch(error => {
+        //             console.log(error);
+        //         });
+        // },
+        // FETCH_ASK({ commit }) {
+        //     fetchAskList()
+        //         .then(({ data }) => {
+        //             commit('SET_ASK', data);
+        //         })
+        //         .catch(error => {
+        //             console.log(error);
+        //         });
+        // },
+    // }
 });
