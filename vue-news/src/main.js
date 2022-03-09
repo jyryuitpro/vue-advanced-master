ebe9e8e77be32f30cbb48ae087a0464379f2c41e
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-09 10:25:08 +0900

    Vuex 모듈화 및 state 적용

--- vue-news/src/main.js
+++ vue-news/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes/index.js';
+import { store } from './store/index.js';
 
 Vue.config.productionTip = false
 
@@ -8,4 +9,6 @@ new Vue({
   render: h => h(App),
   // routes: routes,
   router,
+  // store: store,
+  store,
 }).$mount('#app')
