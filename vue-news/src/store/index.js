6317870f5c2ed7c72d7615f2da025ec3260ec483
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-09 11:26:06 +0900

    [실습] JobsView에 스토어 적용

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,18 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList } from '../api/index.js';
+import { fetchNewsList, fetchJobsList } from '../api/index.js';
+// import { fetchJobsList } from '../api/index.js';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
         news: [],
+        jobs: [],
     },
     mutations: {
         // SET_NEWS: function () {
         SET_NEWS(state, news) {
             state.news = news;
-        }
+        },
+        SET_JOBS(state, jobs) {
+            state.jobs = jobs;
+        },
     },
     actions: {
         FETCH_NEWS(context) {
@@ -24,6 +29,16 @@ export const store = new Vuex.Store({
                 .catch(error => {
                     console.log(error);
                 });
-        }
+        },
+        // 구조 분해 문법(Destructuring)
+        FETCH_JOBS({ commit }) {
+            fetchJobsList()
+                .then(({ data }) => {
+                    commit('SET_JOBS', data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
     }
 });
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div v-for="job in jobs">{{ job.title }}</div>
+<!--    <div v-for="job in jobs">{{ job.title }}</div>-->
+    <div v-for="job in this.$store.state.jobs">{{ job.title }}</div>
   </div>
 </template>
 
@@ -9,15 +10,16 @@ import { fetchJobsList } from '../api/index.js';
 
 export default {
   name: "JobsView",
-  data() {
-    return {
-      jobs: [],
-    }
-  },
+  // data() {
+  //   return {
+  //     jobs: [],
+  //   }
+  // },
   created() {
-    fetchJobsList()
-        .then(response => this.jobs = response.data)
-        .catch(error => console.log(error));
+    this.$store.dispatch('FETCH_JOBS');
+    // fetchJobsList()
+    //     .then(response => this.jobs = response.data)
+    //     .catch(error => console.log(error));
   }
 }
 </script>
