bae72c0d62619ab06e2396409c64db6d07dc9634
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-09 11:07:41 +0900

    NewsView에 actions와 mutations 적용

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,10 +1,29 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { fetchNewsList } from '../api/index.js';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
         news: [],
+    },
+    mutations: {
+        // SET_NEWS: function () {
+        SET_NEWS(state, news) {
+            state.news = news;
+        }
+    },
+    actions: {
+        FETCH_NEWS(context) {
+            fetchNewsList()
+                .then(response => {
+                    console.log(response.data);
+                    context.commit('SET_NEWS', response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
     }
 });
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,32 +1,34 @@
 <template>
   <div>
-    <div v-for="user in users">{{ user.title }}</div>
+<!--    <div v-for="user in users">{{ user.title }}</div>-->
+    <div v-for="user in this.$store.state.news">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
 // import axios from 'axios';
-import { fetchNewsList } from '../api/index.js';
+// import { fetchNewsList } from '../api/index.js';
 
 export default {
   name: "NewsView",
-  data() {
-    return {
-      users: [],
-    }
-  },
+  // data() {
+  //   return {
+  //     users: [],
+  //   }
+  // },
   created() {
-    var vm = this;
-    fetchNewsList()
+    this.$store.dispatch('FETCH_NEWS');
+    // var vm = this;
+    // fetchNewsList()
     // axios.get('https://api.hnpwa.com/v0/news/1.json')
     // new Promise()
-        .then(function (response) {
-          console.log(response);
-          vm.users = response.data;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+    //     .then(function (response) {
+    //       console.log(response);
+    //       vm.users = response.data;
+    //     })
+    //     .catch(function (error) {
+    //       console.log(error);
+    //     });
   }
 }
 </script>
