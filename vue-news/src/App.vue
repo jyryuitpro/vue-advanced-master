8aa96e7b067fc7a94f2f2312288f3ee57c945aae
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-10 15:07:32 +0900

    라우터 트랜지션

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -20,5 +22,13 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+
+/* Router Transition */
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+.page-enter, .page-leave-to /* .page-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
