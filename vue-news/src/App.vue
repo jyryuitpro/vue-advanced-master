f137097bf8bb1ff1238bf7515391ccf580df5d27
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-10 15:38:53 +0900

    [실습 안내] 뉴스 리스트 스타일링

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -23,6 +23,17 @@ body {
   padding: 0;
   margin: 0;
 }
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+a.router-link-exact-active {
+  text-decoration: underline;
+}
 
 /* Router Transition */
 .page-enter-active, .page-leave-active {
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -4,16 +4,44 @@
 <!--    <div v-for="user in this.$store.state.news">-->
 <!--      {{ user.title }}-->
 <!--    </div>-->
-    <p v-for="item in this.$store.state.news">
-      <a v-bind:href="item.url">
-        {{ item.title }}
-      </a>
-      <small>
-        {{ item.time_ago }} by 
+    <ul class="news-list">
+      <li v-for="item in this.$store.state.news" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a v-bind:href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+          </small>
+        </div>
+<!--        <a v-bind:href="item.url">
+          {{ item.title }}
+        </a>
+        <small>
+          {{ item.time_ago }} by
+          <router-link v-bind:to="'/user/' + item.user">{{ item.user }}</router-link>
+          <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
+        </small>-->
+      </li>
+    </ul>
+<!--    <p v-for="item in this.$store.state.news">-->
+<!--      <a v-bind:href="item.url">-->
+<!--        {{ item.title }}-->
+<!--      </a>-->
+<!--      <small>-->
+<!--        {{ item.time_ago }} by -->
 <!--        <router-link v-bind:to="'/user/' + item.user">{{ item.user }}</router-link>-->
-        <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
-      </small>
-    </p>
+<!--        <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>-->
+<!--      </small>-->
+<!--    </p>-->
   </div>
 </template>
 
@@ -46,5 +74,28 @@ export default {
 </script>
 
 <style scoped>
-
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
