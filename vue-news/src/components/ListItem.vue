09368f7d1ab410e02bbab85e157566bf99dade28
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-03-10 16:28:20 +0900

    [실습 안내] 공통 컴포넌트 ListItem 제작 및 실습 안내

---
+++ vue-news/src/components/ListItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <ul class="news-list">
+      <li v-for="item in this.$store.state.news" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a v-bind:href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <router-link v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  created() {
+    this.$store.dispatch('FETCH_NEWS');
+  },
+}
+</script>
+
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
+    <list-item></list-item>
 <!--    <div v-for="user in users">{{ user.title }}</div>-->
 <!--    <div v-for="user in this.$store.state.news">-->
 <!--      {{ user.title }}-->
 <!--    </div>-->
-    <ul class="news-list">
+<!--<ul class="news-list">
       <li v-for="item in this.$store.state.news" class="post">
-        <!-- 포인트 영역 -->
         <div class="points">
           {{ item.points }}
         </div>
-        <!-- 기타 정보 영역 -->
         <div>
           <p class="news-title">
             <a v-bind:href="item.url">
@@ -21,7 +20,7 @@
             {{ item.time_ago }} by
             <router-link v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
           </small>
-        </div>
+        </div>-->
 <!--        <a v-bind:href="item.url">
           {{ item.title }}
         </a>
@@ -30,8 +29,8 @@
           <router-link v-bind:to="'/user/' + item.user">{{ item.user }}</router-link>
           <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
         </small>-->
-      </li>
-    </ul>
+<!--      </li>-->
+<!--    </ul>-->
 <!--    <p v-for="item in this.$store.state.news">-->
 <!--      <a v-bind:href="item.url">-->
 <!--        {{ item.title }}-->
@@ -48,16 +47,21 @@
 <script>
 // import axios from 'axios';
 // import { fetchNewsList } from '../api/index.js';
+import ListItem from '../components/ListItem.vue';
 
 export default {
   name: "NewsView",
+  components: {
+    // ListItem: ListItem
+    ListItem
+  }
   // data() {
   //   return {
   //     users: [],
   //   }
   // },
-  created() {
-    this.$store.dispatch('FETCH_NEWS');
+  // created() {
+  //   this.$store.dispatch('FETCH_NEWS');
     // var vm = this;
     // fetchNewsList()
     // axios.get('https://api.hnpwa.com/v0/news/1.json')
@@ -69,33 +73,10 @@ export default {
     //     .catch(function (error) {
     //       console.log(error);
     //     });
-  }
+  // }
 }
 </script>
 
 <style scoped>
-.news-list {
-  margin: 0;
-  padding: 0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-.news-title {
-  margin: 0;
-}
-.link-text {
-  color: #828282;
-}
+
 </style>
